c43e882fb7b84324778dc1b1a67609b3afa2579b
DennKK <karpovdenis.al@gmail.com>
2021-12-28 23:44:07 +0400

    add app

--- movie_site/main/models.py
+++ movie_site/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    # Date Of Birth
+    DOB = models.DateField()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Genre(models.Model):
+    name = models.CharField(default='IDONTKNOW', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    genre = models.ManyToManyField(Genre)
+    description = models.TextField()
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    age_limit = models.IntegerField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
