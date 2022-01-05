9ca58cba45859211845b3f3a8e4a4afb3d443dbe
DennKK <karpovdenis.al@gmail.com>
2022-01-06 00:10:58 +0400

    update

--- movie_site/movie_site/settings.py
+++ movie_site/movie_site/settings.py
@@ -121,10 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
